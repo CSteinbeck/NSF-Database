d662ad5d9b3ca7aab5af2344c5c692a0e6ae6b09
WillH97 <71902196+WillH97@users.noreply.github.com>
2020-10-17 21:30:13 -0400

    Update SqlServerDbAccessor.java

--- Analytics/SqlServerDbAccessor.java
+++ Analytics/SqlServerDbAccessor.java
@@ -1,3 +1,19 @@
+/* SqlServerDbAccessor Class
+   This class connects you to the SQL Server so that you can qeury to your heart's content
+   
+   General Format for connecting to db in your client code:
+   SqlServerDbAccessor dba = new SqlServerDbAccessor();
+   dba.setDbName("name of database");
+   dba.connectToDb();
+   
+   The default constructor uses the default connection URL which, at the moment, is the 
+   login credentials for an admin. If you do not want to connect as an admin, use the other
+   constructor. Login credentials can be found on Discord.
+
+   After declaring a database accessor object, read the SelectTest or CustomQueryTest for 
+   how to query the database.
+*/
+
 package analytics;
 
 import java.sql.Connection;
@@ -9,11 +25,8 @@ public class SqlServerDbAccessor {
 	
 	private String defaultConnUrl = "jdbc:sqlserver://;" +
             "servername=csdata.cd4sevot432y.us-east-1.rds.amazonaws.com;"
-//			+ "user=csc312cloud;password=c3s!c2Cld;";
-//            + "user=csc480dev;password=c4s*C0sWe;"; 
-//			+ "user=NsfResearcher;password=r2h0e2S0u5s;";
 			+ "user=administrator;password=MercerBear$2020;";
-			// + "databaseName=JLBookstore;";
+	
 	/*
 	// in WSC
 	private String defaultConnUrl = "jdbc:sqlserver://;servername=cssql\\sqldata;"
@@ -21,10 +34,12 @@ public class SqlServerDbAccessor {
 			"databaseName=JLBookstore;";
 	*/
 
+	// Default constructor sets the login credentials to admin
 	public SqlServerDbAccessor() {
 		connectionUrl = defaultConnUrl;
 	}
 	
+	// This constructor lets you choose username and password
 	public SqlServerDbAccessor(String serverName, String user, String pwd, String dbName) {
 		connectionUrl = "jdbc:sqlserver://;";
 		connectionUrl += "servername=" + serverName + ";"; 
@@ -38,14 +53,13 @@ public class SqlServerDbAccessor {
 	}
 	
 	public void connectToDb() {
-    	try {
-    		// Establish the connection.
-    		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        	con = DriverManager.getConnection(connectionUrl);
-    	} catch (Exception e) {
+    		try {
+    			// Establish the connection.
+    			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        		con = DriverManager.getConnection(connectionUrl);
+    		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	public Connection getConnection() {
